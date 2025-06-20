277c12135797355cadcd262efe57a2bc53bcd488
anonymousboy67 <aashishad67@gmail.com>
2025-06-20 11:53:06 +0545

    Updating UserController with Status Codes

--- src/main/java/com/codewithaashish/mavenproject/UserController.java
+++ src/main/java/com/codewithaashish/mavenproject/UserController.java
@@ -2,10 +2,9 @@ package com.codewithaashish.mavenproject;
 
 
 import com.codewithaashish.mavenproject.model.User;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -33,10 +32,31 @@ public class UserController{
     }
 
     //Get /api/users/{id} - GET user by id
+//    @GetMapping("/{id}")
+//    public User getUserById(@PathVariable Long id){
+//        return users.stream().filter(user->user.getId().equals(id)).findFirst().orElse(null);
+//    }
+
     @GetMapping("/{id}")
-    public User getUserById(@PathVariable Long id){
-        return users.stream().filter(user->user.getId().equals(id)).findFirst().orElse(null);
+    public ResponseEntity<User> getUserById(@PathVariable Long id){
+        User user=users.stream().filter(u->u.getId().equals(id)).findFirst().orElse(null);
+
+        if(user !=null){
+            return ResponseEntity.ok(user);
+        }else{
+            return ResponseEntity.notFound().build();
+        }
+
     }
+
+    //Add a POST method for creating users
+    @PostMapping
+    public ResponseEntity<User> createUser(@RequestBody User user){
+        user.setId(nextId++);
+        users.add(user);
+        return ResponseEntity.status(HttpStatus.CREATED).body(user);
+    }
+
 
 
     @GetMapping("/search/{name}")
